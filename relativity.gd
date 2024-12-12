ee84859586683f9dfa2f5a3971e6880c418b868f
albizgil <albizgil@gmail.com>
2024-12-12 23:43:46 +0100

    Removed unused uniforms. Added gamepad movement.

--- relativity.gd
+++ relativity.gd
@@ -7,6 +7,12 @@ var uposition = Vector4.ZERO
 var fourvel = Vector4(0,0,0,1)
 var boost = Vector4.ZERO
 var transform_matrix = Projection.IDENTITY
+var view2d = Vector2(0.5,0.5)
+var viewForward = Vector3(1,0,0)
+var viewUp = Vector3(0,1,0)
+var viewRight = Vector3(0,0,1)
+
+var lookSpeed = 1.0
 
 var shader_transformMatrix
 var shader_position
@@ -14,10 +20,13 @@ var shader_orientation
 var shader_fourvel
 var shader_boost
 var shader_iResolution
-var shader_iMouse
-var shader_iTimeDelta
+var shader_view2d
+var shader_viewForward
+var shader_viewUp
+var shader_viewRight
 
-var is_slow = false
+var time_scale = 1.0
+var speedOfLight = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -29,8 +38,11 @@ func _ready() -> void:
 	shader_boost = StringName("boost")
 	shader_orientation = StringName("orientation")
 	shader_iResolution = StringName("iResolution")
-	shader_iMouse = StringName("iMouse")
-	shader_iTimeDelta = StringName("iTimeDelta")
+	shader_view2d = StringName("view2d")
+	shader_viewForward = StringName("viewForward")
+	shader_viewUp = StringName("viewUp")
+	shader_viewRight = StringName("viewRight")
+	
 	
 	#var resolution = Vector3(get_viewport().size.x, get_viewport().size.y, 1.0)
 	var resolution = Vector3(1920, 1920, 1.0)
@@ -68,24 +80,64 @@ func update_shader_values() -> void:
 	material.set_shader_parameter(shader_orientation, orientation)
 	material.set_shader_parameter(shader_boost, boost)
 	material.set_shader_parameter(shader_transformMatrix, transform_matrix)
+	material.set_shader_parameter(shader_view2d, view2d)
+	material.set_shader_parameter(shader_viewForward, viewForward)
+	material.set_shader_parameter(shader_viewUp, viewUp)
+	material.set_shader_parameter(shader_viewRight, viewRight)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var c = cos(.1*delta)
+	var s = sin(.1*delta)	
+	orientation = Vector3(c*orientation.x-s*orientation.y,
+	s*orientation.x+c*orientation.y,0);	
+	
 	var delta_scaled = delta
-	if is_slow:
-		delta_scaled /= 10
-	handle_physics(delta_scaled, Vector4(.1, 0, 0, 1), 1)
+	
+	if Input.is_action_pressed("SlowDown"):
+		slow_down(delta)
+	else:
+		speed_up(delta)
+	
+	delta_scaled /= time_scale
+	
+	var boostBackForward = Input.get_axis("BoostBack", "BoostForward")
+	var boostLeftRight = Input.get_axis("BoostLeft", "BoostRight")
+	var boostUpDown = Input.get_axis("BoostDown", "BoostUp")
+	var boostVector = boostBackForward * viewForward + boostLeftRight * viewRight +	boostUpDown * viewUp
+	
+	handle_physics(
+		delta_scaled,
+		Vector4(boostVector.x, boostVector.y, boostVector.z, 1.0),
+		speedOfLight
+	)
+	
+	viewForward += delta * lookSpeed * Input.get_axis("LookLeft", "LookRight") * viewRight
+	viewForward += delta * lookSpeed * Input.get_axis("LookDown", "LookUp") * viewUp
+	viewForward = viewForward.normalized()	
+	
+	viewUp = viewRight.cross(viewForward)
+	viewUp = viewUp.normalized()
+	viewRight = viewForward.cross(viewUp)
 	
 	update_shader_values()
 
-func toggle_speed() -> void:
-	is_slow = not is_slow
+func slow_down(delta : float):
+	if time_scale < 10.0:
+		time_scale += delta * 5.0
+	else:
+		time_scale = 10.0
 
+func speed_up(delta : float):
+	if (time_scale > 1.0):
+		time_scale -= delta * 5.0
+	else:
+		time_scale = 1.0
 
-func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed:
-			if event.keycode == KEY_ESCAPE:
-				get_tree().quit()
-			elif event.keycode == KEY_SPACE:
-				toggle_speed()
+func Rotate2D(vec : Vector2, angle : float) -> Vector2:
+	var T = Transform2D(
+		Vector2(cos(angle),sin(angle)),
+		Vector2(-sin(angle), cos(angle)),
+		Vector2.ZERO
+	)
+	return T * vec
